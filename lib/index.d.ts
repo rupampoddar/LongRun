d120ba3ce9026394b3a7f7ccaa55e874abbbbf80
Rupam Poddar <rupam.poddar@protonmail.com>
2021-11-18 10:38:19 +0530

    implement alternate trigger logic

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,19 +1,32 @@
-/// <reference types="google-apps-script" />
-/**
- * Long-Running Support
- */
-import Properties = GoogleAppsScript.Properties.Properties;
+declare type EveryMinutesType = 1 | 5 | 10 | 15 | 30;
+declare type ExecScopeType = "user" | "script" | "document";
+export declare type SetupOptions = {
+    execScope: ExecScopeType;
+    taskCount: number;
+    maxRuntime: number;
+    triggerEveryNMinutes: EveryMinutesType;
+};
 export declare class LongRun {
     private static _instance;
     static PREFIX_RUNNING: string;
-    static PREFIX_TRIGGER_KEY: string;
+    static PREFIX_TRIGGER_ID: string;
     static PREFIX_START_POS: string;
+    static PREFIX_CURRENT_POS: string;
     static PREFIX_OPTION: string;
+    static PREFIX_OPTION_EXEC_SCOPE: string;
+    static PREFIX_OPTION_TASK_COUNT: string;
+    static PREFIX_OPTION_MAX_RUNTIME: string;
+    static PREFIX_OPTION_TRIGGER_EVERY_N_MINS: string;
+    static PREFIX_FUNC_ARGS: string;
+    static PREFIX_TASK_COUNT: string;
+    static PREFIX_TASK_COMPLETED_INDEX: string;
     static RUNNING_MAX_SECONDS: number;
     static RUNNING_DELAY_MINUTES: number;
-    properties: Properties;
+    private properties;
+    startTimeMap: any;
     /**
-     * Private constructor
+     * Constructor
+     *
      * @private
      */
     private constructor();
@@ -21,8 +34,40 @@ export declare class LongRun {
      * Returns singleton instance.
      */
     static get instance(): LongRun;
-    /** start time map */
-    startTimeMap: any;
+    setup(funcName: string, options: SetupOptions, funcArgs?: any[]): void;
+    /**
+     *
+     *
+     * @returns start index ( 0 for the first time )
+     */
+    /**
+     * This function is to be called from the long running function.
+     * It returns the index from where the loop needs to be started.
+     *
+     * @param funcName name of the long running function
+     * @returns index of the process or iteration
+     */
+    getStartIndex(funcName: string): number;
+    /**
+     * Determines whether the process should be suspended based on elapsed time.
+     *
+     * @param funcName
+     * @param currentIndex - start position when resuming
+     * @return true - it should be suspended
+     */
+    checkShouldSuspend(funcName: string, currentIndex: number): boolean;
+    setTaskCompleted(funcName: string, index: number): void;
+    /**
+     * Resets Long-Running variables if there is no next trigger.
+     * Returns whether the command has finished or not.
+     * @param funcName
+     */
+    end(funcName: string): boolean;
+    /**
+     * Resets Long-Running variables
+     * @param funcName
+     */
+    reset(funcName: string): void;
     /**
      * Returns if function is running now.
      * @param funcName
@@ -30,10 +75,11 @@ export declare class LongRun {
     isRunning(funcName: string): boolean;
     /**
      * Sets the function is running
-     * @param funcName
-     * @param running
+     *
+     * @param funcName the name of the long running function
+     * @param isRunning true or false
      */
-    setRunning(funcName: string, running: boolean): void;
+    setRunning(funcName: string, isRunning: boolean): void;
     /**
      * Sets max execution seconds
      * @param seconds
@@ -48,37 +94,13 @@ export declare class LongRun {
      * Returns the function parameters
      * @param funcName
      */
-    getParameters(funcName: string): string[];
+    getSetupOptions(funcName: string): SetupOptions | null;
     /**
      * Sets the function parameters.
      * @param funcName
      * @param parameters
      */
     setParameters(funcName: string, parameters: string[]): void;
-    /**
-     * Starts or Resume Long-Run process.
-     * @returns start index ( 0 for the first time )
-     */
-    startOrResume(funcName: string): number;
-    /**
-     * Determines whether the process should be suspended.
-     * If it should be suspended, the next trigger will be registered.
-     * @param funcName
-     * @param nextIndex - start position when resuming
-     * @return true - it should be suspended
-     */
-    checkShouldSuspend(funcName: string, nextIndex: number): boolean;
-    /**
-     * Resets Long-Running variables
-     * @param funcName
-     */
-    reset(funcName: string): void;
-    /**
-     * Resets Long-Running variables if there is no next trigger.
-     * Returns whether the command has finished or not.
-     * @param funcName
-     */
-    end(funcName: string): boolean;
     /**
      * Returns if there is next trigger.
      * @param funcName
@@ -92,6 +114,7 @@ export declare class LongRun {
     registerNextTrigger(funcName: string, nextIndex: number): void;
     /**
      * Deletes the trigger
+     *
      * @param triggerKey
      */
     private deleteTrigger;
@@ -102,3 +125,4 @@ export declare class LongRun {
      */
     private setTrigger;
 }
+export {};
--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,61 @@
-/**
- * Long-Running Support
- */
 import Properties = GoogleAppsScript.Properties.Properties;
+
+type EveryMinutesType = 1 | 5 | 10 | 15 | 30;
+type ExecScopeType = "user" | "script" | "document";
+
+export type SetupOptions = {
+  execScope: ExecScopeType;
+  taskCount: number;
+  maxRuntime: number;
+  triggerEveryNMinutes: EveryMinutesType;
+};
+class GasTrigger {
+  constructor(functionName: string, everyMinutes: EveryMinutesType) {
+    //
+    // https://developers.google.com/apps-script/reference/script/clock-trigger-builder#everyminutesn
+    return ScriptApp.newTrigger(functionName)
+      .timeBased()
+      .everyMinutes(everyMinutes) // trigger every n minutes. n must be 1, 5, 10, 15 or 30.
+      .create();
+  }
+}
 
 export class LongRun {
   // singleton instance
   private static _instance: LongRun;
 
   // constants
-  static PREFIX_RUNNING: string = 'running_';
-  static PREFIX_TRIGGER_KEY: string = 'trigger_';
-  static PREFIX_START_POS: string = 'start_';
-  static PREFIX_OPTION: string = 'option_';
+  static PREFIX_RUNNING: string = "running_"; // in use
+  static PREFIX_TRIGGER_ID: string = "trigger_"; // in use
+  static PREFIX_START_POS: string = "start_";
+  static PREFIX_CURRENT_POS: string = "current_";
+  static PREFIX_OPTION: string = "option_";
+
+  // in use
+  static PREFIX_OPTION_EXEC_SCOPE: string = "option_exec_scope_";
+  static PREFIX_OPTION_TASK_COUNT: string = "option_task_count_";
+  static PREFIX_OPTION_MAX_RUNTIME: string = "option_MAX_RUNTIME_";
+  static PREFIX_OPTION_TRIGGER_EVERY_N_MINS: string = "option_trigger_every_n_minutes_";
+
+  static PREFIX_FUNC_ARGS: string = "args_"; // in use
+
+  static PREFIX_TASK_COUNT: string = "task_count_";
+
+  static PREFIX_TASK_COMPLETED_INDEX: string = "task_completed_index_"; // in use
+
   static RUNNING_MAX_SECONDS: number = 4 * 60;
   static RUNNING_DELAY_MINUTES: number = 1;
 
-  properties: Properties;
+  private properties: Properties;
+  startTimeMap: any = {}; // start time map
 
   /**
-   * Private constructor
+   * Constructor
+   *
    * @private
    */
-  private constructor(scope: string | null = null) {
-    switch (scope) {
-      case 'script':
-        this.properties = PropertiesService.getScriptProperties();
-        break;
-      case 'document':
-        this.properties = PropertiesService.getDocumentProperties();
-        break;
-      default:
-        this.properties = PropertiesService.getUserProperties();
-        break;
-    }
+  private constructor() {
+    this.properties = PropertiesService.getUserProperties();
   }
 
   /**
@@ -45,29 +68,181 @@ export class LongRun {
     return this._instance;
   }
 
-  /** start time map */
-  startTimeMap: any = {};
+  setup(funcName: string, options: SetupOptions, funcArgs: any[] = []) {
+    const {
+      execScope,
+      taskCount, // total task count (i.e. loop count)
+      maxRuntime, // maximum acceptable run time in seconds
+      triggerEveryNMinutes, // trigger the long running function every n minutes
+    } = options;
+
+    // if (execScope === 'script') {
+    //   this.properties = PropertiesService.getScriptProperties();
+    // } else if (execScope === 'document') {
+    //   this.properties = PropertiesService.getDocumentProperties();
+    // } else if (execScope === 'user') {
+    //   this.properties = PropertiesService.getUserProperties();
+    // }
+
+    // https://developers.google.com/apps-script/reference/script/clock-trigger-builder#everyminutesn
+    const triggerId = ScriptApp.newTrigger(funcName)
+      .timeBased()
+      .everyMinutes(triggerEveryNMinutes) // trigger every n minutes. n must be 1, 5, 10, 15 or 30.
+      .create()
+      .getUniqueId();
+
+    // set properties in batch.
+    // (using es2015 computed property names)
+    this.properties.setProperties({
+      [LongRun.PREFIX_OPTION_EXEC_SCOPE + funcName]: execScope,
+      [LongRun.PREFIX_OPTION_TASK_COUNT + funcName]: String(taskCount),
+      [LongRun.PREFIX_OPTION_MAX_RUNTIME + funcName]: String(maxRuntime),
+      [LongRun.PREFIX_OPTION_TRIGGER_EVERY_N_MINS + funcName]: String(triggerEveryNMinutes),
+      [LongRun.PREFIX_FUNC_ARGS + funcName]: JSON.stringify(funcArgs),
+      [LongRun.PREFIX_TRIGGER_ID + funcName]: triggerId,
+    });
+  }
+
+  /**
+   *
+   *
+   * @returns start index ( 0 for the first time )
+   */
+
+  /**
+   * This function is to be called from the long running function.
+   * It returns the index from where the loop needs to be started.
+   *
+   * @param funcName name of the long running function
+   * @returns index of the process or iteration
+   */
+  getStartIndex(funcName: string): number {
+    // save start time
+    this.startTimeMap[funcName] = new Date().getTime();
+
+    // set running flag
+    this.setRunning(funcName, true);
+
+    // if the trigger exists, delete it.
+    // this.deleteTrigger(LongRun.PREFIX_TRIGGER_KEY + funcName);
+
+    // calculate start index
+    let lastCompletedProcessIndex: number | string | null = this.properties.getProperty(
+      LongRun.PREFIX_TASK_COMPLETED_INDEX + funcName
+    );
+    if (lastCompletedProcessIndex !== null) {
+      return parseInt(lastCompletedProcessIndex) + 1;
+    } else {
+      return 0;
+    }
+  }
+
+  /**
+   * Determines whether the process should be suspended based on elapsed time.
+   *
+   * @param funcName
+   * @param currentIndex - start position when resuming
+   * @return true - it should be suspended
+   */
+  checkShouldSuspend(funcName: string, currentIndex: number): boolean {
+    // get the (long-running) function start time
+    const startTime = this.startTimeMap[funcName];
+    // calculate elapsed time (in seconds)
+    const elapsedTime = (new Date().getTime() - startTime) / 1000;
+
+    // if it's past the specified time, suspend the process
+    if (elapsedTime >= LongRun.RUNNING_MAX_SECONDS) {
+      // register the next trigger and set running-flag off
+      // this.registerNextTrigger(funcName, nextIndex);
+
+      // update the start index so that on next run it will
+      // pick up from this index
+      // this.properties.setProperty(
+      //   LongRun.PREFIX_START_POS + funcName,
+      //   String(currentIndex)
+      // );
+      // turn off running flag
+      this.properties.deleteProperty(LongRun.PREFIX_RUNNING + funcName);
+
+      return true;
+    } else {
+      // update current process index
+      // this.properties.setProperty(
+      //   LongRun.PREFIX_CURRENT_POS + funcName,
+      //   String(currentIndex)
+      // );
+      return false;
+    }
+  }
+
+  setTaskCompleted(funcName: string, index: number): void {
+    this.properties.setProperty(LongRun.PREFIX_TASK_COMPLETED_INDEX + funcName, String(index));
+  }
+
+  /**
+   * Resets Long-Running variables if there is no next trigger.
+   * Returns whether the command has finished or not.
+   * @param funcName
+   */
+  end(funcName: string): boolean {
+    // let ret: boolean = false;
+    // if (!this.existsNextTrigger(functionName)) {
+    //   this.reset(functionName);
+    //   ret = true;
+    // }
+    // return ret;
+    // check if all the processes are completed
+    const taskCount = this.properties.getProperty(LongRun.PREFIX_OPTION_TASK_COUNT + funcName);
+    const completedTaskIndex = this.properties.getProperty(
+      LongRun.PREFIX_TASK_COMPLETED_INDEX + funcName
+    );
+    if (taskCount !== null && completedTaskIndex !== null) {
+      if (parseInt(taskCount) === parseInt(completedTaskIndex) - 1) {
+        this.reset(funcName);
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Resets Long-Running variables
+   * @param funcName
+   */
+  reset(funcName: string): void {
+    // delete trigger
+    this.deleteTrigger(LongRun.PREFIX_TRIGGER_ID + funcName);
+
+    // delete properties
+    this.properties.deleteProperty(LongRun.PREFIX_OPTION_EXEC_SCOPE + funcName);
+    this.properties.deleteProperty(LongRun.PREFIX_OPTION_TASK_COUNT + funcName);
+    this.properties.deleteProperty(LongRun.PREFIX_OPTION_MAX_RUNTIME + funcName);
+    this.properties.deleteProperty(LongRun.PREFIX_OPTION_TRIGGER_EVERY_N_MINS + funcName);
+    this.properties.deleteProperty(LongRun.PREFIX_FUNC_ARGS+funcName)
+    this.properties.deleteProperty(LongRun.PREFIX_TASK_COMPLETED_INDEX+funcName)
+    this.properties.deleteProperty(LongRun.PREFIX_RUNNING + funcName);
+    this.properties.deleteProperty(LongRun.PREFIX_TRIGGER_ID + funcName);
+  }
 
   /**
    * Returns if function is running now.
    * @param funcName
    */
   isRunning(funcName: string): boolean {
-    let running: string | null = this.properties.getProperty(
-      LongRun.PREFIX_RUNNING + funcName
-    );
-    return !(running == null || running === '');
+    let running: string | null = this.properties.getProperty(LongRun.PREFIX_RUNNING + funcName);
+    return !(running == null || running === "");
   }
 
   /**
    * Sets the function is running
-   * @param funcName
-   * @param running
+   *
+   * @param funcName the name of the long running function
+   * @param isRunning true or false
    */
-  setRunning(funcName: string, running: boolean): void {
+  setRunning(funcName: string, isRunning: boolean): void {
     const key = LongRun.PREFIX_RUNNING + funcName;
-    if (running) {
-      this.properties.setProperty(key, 'running');
+    if (isRunning) {
+      this.properties.setProperty(key, "running");
     } else {
       this.properties.deleteProperty(key);
     }
@@ -92,15 +267,33 @@ export class LongRun {
    * Returns the function parameters
    * @param funcName
    */
-  getParameters(funcName: string): string[] {
-    let parameters = this.properties.getProperty(
-      LongRun.PREFIX_OPTION + funcName
+  getSetupOptions(funcName: string): SetupOptions | null {
+    // let parameters = this.properties.getProperty(LongRun.PREFIX_OPTION + funcName);
+    // if (parameters != null) {
+    //   return parameters.split(",");
+    // } else {
+    //   return [];
+    // }
+    const execScope = this.properties.getProperty(LongRun.PREFIX_OPTION_EXEC_SCOPE + funcName);
+    const taskCount = this.properties.getProperty(LongRun.PREFIX_OPTION_TASK_COUNT + funcName);
+    const maxRuntime = this.properties.getProperty(LongRun.PREFIX_OPTION_MAX_RUNTIME + funcName);
+    const triggerEveryNMinutes = this.properties.getProperty(
+      LongRun.PREFIX_OPTION_TRIGGER_EVERY_N_MINS + funcName
     );
-    if (parameters != null) {
-      return parameters.split(',');
-    } else {
-      return [];
+    if (
+      execScope !== null &&
+      taskCount !== null &&
+      maxRuntime !== null &&
+      triggerEveryNMinutes !== null
+    ) {
+      return {
+        execScope: execScope as ExecScopeType,
+        taskCount: parseInt(taskCount),
+        maxRuntime: parseInt(maxRuntime),
+        triggerEveryNMinutes: parseInt(triggerEveryNMinutes) as EveryMinutesType,
+      };
     }
+    return null;
   }
   /**
    * Sets the function parameters.
@@ -109,86 +302,10 @@ export class LongRun {
    */
   setParameters(funcName: string, parameters: string[]): void {
     if (parameters != null) {
-      this.properties.setProperty(
-        LongRun.PREFIX_OPTION + funcName,
-        parameters.join(',')
-      );
+      this.properties.setProperty(LongRun.PREFIX_OPTION + funcName, parameters.join(","));
     } else {
       this.properties.deleteProperty(LongRun.PREFIX_OPTION + funcName);
     }
-  }
-
-  /**
-   * Starts or Resume Long-Run process.
-   * @returns start index ( 0 for the first time )
-   */
-  startOrResume(funcName: string): number {
-    // save start time
-    this.startTimeMap[funcName] = new Date().getTime();
-
-    // set running-flag
-    this.setRunning(funcName, true);
-
-    // if the trigger exists, delete it.
-    this.deleteTrigger(LongRun.PREFIX_TRIGGER_KEY + funcName);
-
-    // get start index
-    let startPos: number = parseInt(
-      this.properties.getProperty(LongRun.PREFIX_START_POS + funcName) || '0'
-    );
-    if (!startPos) {
-      return 0;
-    } else {
-      return startPos;
-    }
-  }
-
-  /**
-   * Determines whether the process should be suspended.
-   * If it should be suspended, the next trigger will be registered.
-   * @param funcName
-   * @param nextIndex - start position when resuming
-   * @return true - it should be suspended
-   */
-  checkShouldSuspend(funcName: string, nextIndex: number): boolean {
-    let startTime = this.startTimeMap[funcName];
-    let diff = (new Date().getTime() - startTime) / 1000;
-    // If it's past the specified time, suspend the process
-    if (diff >= LongRun.RUNNING_MAX_SECONDS) {
-      // register the next trigger and set running-flag off
-      this.registerNextTrigger(funcName, nextIndex);
-
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  /**
-   * Resets Long-Running variables
-   * @param funcName
-   */
-  reset(funcName: string): void {
-    // delete trigger
-    this.deleteTrigger(LongRun.PREFIX_TRIGGER_KEY + funcName);
-    this.properties.deleteProperty(LongRun.PREFIX_START_POS + funcName);
-    this.properties.deleteProperty(LongRun.PREFIX_OPTION + funcName);
-    this.properties.deleteProperty(LongRun.PREFIX_RUNNING + funcName);
-    this.properties.deleteProperty(LongRun.PREFIX_TRIGGER_KEY + funcName);
-  }
-
-  /**
-   * Resets Long-Running variables if there is no next trigger.
-   * Returns whether the command has finished or not.
-   * @param funcName
-   */
-  end(funcName: string): boolean {
-    let ret: boolean = false;
-    if (!this.existsNextTrigger(funcName)) {
-      this.reset(funcName);
-      ret = true;
-    }
-    return ret;
   }
 
   /**
@@ -196,9 +313,7 @@ export class LongRun {
    * @param funcName
    */
   existsNextTrigger(funcName: string): boolean {
-    let triggerId = this.properties.getProperty(
-      LongRun.PREFIX_TRIGGER_KEY + funcName
-    );
+    let triggerId = this.properties.getProperty(LongRun.PREFIX_TRIGGER_ID + funcName);
     return triggerId != null;
   }
 
@@ -208,11 +323,8 @@ export class LongRun {
    * @param nextIndex - start position when resuming
    */
   registerNextTrigger(funcName: string, nextIndex: number): void {
-    this.properties.setProperty(
-      LongRun.PREFIX_START_POS + funcName,
-      String(nextIndex)
-    ); // save next start position
-    this.setTrigger(LongRun.PREFIX_TRIGGER_KEY + funcName, funcName); // set trigger
+    this.properties.setProperty(LongRun.PREFIX_START_POS + funcName, String(nextIndex)); // save next start position
+    this.setTrigger(LongRun.PREFIX_TRIGGER_ID + funcName, funcName); // set trigger
 
     // turn off running-flag
     this.properties.deleteProperty(LongRun.PREFIX_RUNNING + funcName);
@@ -220,13 +332,17 @@ export class LongRun {
 
   /**
    * Deletes the trigger
+   *
    * @param triggerKey
    */
   private deleteTrigger(triggerKey: string): void {
-    let triggerId = this.properties.getProperty(triggerKey);
+    const triggerId = this.properties.getProperty(triggerKey);
 
     if (!triggerId) return;
 
+    console.log(ScriptApp.getProjectTriggers())
+    // get the function execution scope
+    // const execScope = this.properties.getProperty;
     ScriptApp.getProjectTriggers()
       .filter(function (trigger) {
         return trigger.getUniqueId() == triggerId;
@@ -235,6 +351,8 @@ export class LongRun {
         ScriptApp.deleteTrigger(trigger);
       });
     this.properties.deleteProperty(triggerKey);
+
+
   }
 
   /**
@@ -246,11 +364,7 @@ export class LongRun {
     this.deleteTrigger(triggerKey); // delete if exists.
     let dt: Date = new Date();
     dt.setMinutes(dt.getMinutes() + LongRun.RUNNING_DELAY_MINUTES); // will execute after the specified time
-    let triggerId = ScriptApp.newTrigger(funcName)
-      .timeBased()
-      .at(dt)
-      .create()
-      .getUniqueId();
+    let triggerId = ScriptApp.newTrigger(funcName).timeBased().at(dt).create().getUniqueId();
     // save the trigger id to delete the trigger later.
     this.properties.setProperty(triggerKey, triggerId);
   }
